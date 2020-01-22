b051c04698167a7a0059146d255ebb8f8c39fe2e
Dale Ramdeen <dramdeen1@gmail.com>
2020-01-22 12:16:36 -0500

    quick push changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hashi_sentinel_policies
-Training materials for exploring Hashi sentinel Policies 
+Starter repo for bootstraping Terraform Enterprise with simple Sentinel policies.
+
+By executing this terraform code, you will introduce new Sentinel policies and policy sets to your workspace of choice.
+
---
+++ main.tf
@@ -0,0 +1,26 @@
+
+
+variable "tfe_hostname" {
+    description = "The domain where your TFE is hosted."
+    default     = "app.terraform.io"
+}
+
+variable "tfe_organization" {
+    description = "The TFE organization to apply your changes to."
+    default     = "example_corp"
+}
+
+provider "azurerm" {
+    # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+    version = "=1.38.0"
+    token    = var.tfe_token
+    version  = "~> 0.6"
+}
+
+resource "tfe_sentinel_policy" "main" {
+    name         = "enforce-mandatory-tags"
+    description  = "Don't allow resources to be created without tags"
+    organization = var.tfe_token
+    policy       = file("${path.module}/sentinel/enforce-mandatory-tags.sentinel)
+    enforce_mode = "hard-mandatory"
+}
